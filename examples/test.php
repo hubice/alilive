49cea861d109e96ef2605799f3b82535515a77e7
naice <1820120473@qq.com>
2019-06-28 15:11:40 +0800

    test

--- examples/test.php
+++ examples/test.php
@@ -10,13 +10,5 @@ date_default_timezone_set("PRC");
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$live = new \AliLive\Task(
-            '',
-    '42784.livepush.myqcloud.com',
-    'live.pan233.com',
-    '123456789',
-    '987654321',
-    'a');
 
-$live->liveChannelSetStatus(1,0);
 
